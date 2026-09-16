9ad45822bbc9492ec07f202d7a779ce8b22f862a
Akhil Balthi <b.akhil667@gmail.com>
2026-09-16 10:14:30 +0530

    Use dynamic PORT for deployment

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,8 @@ require('dotenv').config()
 
 const app = require("./src/app");
 
-app.listen(3000, () => {
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
   console.log("Server is running on PORT 3000");
 });
